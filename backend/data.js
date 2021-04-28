9dbb44cb20ddfbd286d0dd91831729a3b95ca680
Dovydas Savickas <dagas199@gmail.com>
2021-04-28 20:56:50 +0200

    backend server created

---
+++ backend/data.js
@@ -0,0 +1,29 @@
+const data = {
+  notifications: [
+    {
+      title: "New message",
+    },
+    {
+      title: "Your details have been updated",
+    },
+    {
+      title: "You got rated",
+    },
+    {
+      title: "You've been reported",
+    },
+    {
+      title: "You have a new message",
+    },
+    {
+      title: "Your details have been updated",
+    },
+    {
+      title: "You got rated",
+    },
+    {
+      title: "You've been reported",
+    },
+  ],
+};
+export default data;
